d8272b6076d799f72b67f0f4814b77d47e3c7360
sanzdecastro <sanzdecastro@gmail.com>
2024-04-27 21:12:42 +0200

    test

--- api/server.js
+++ api/server.js
@@ -1,17 +1,47 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Your database file
-const middlewares = jsonServer.defaults();
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-// Add custom middleware for CORS
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+const server = jsonServer.create()
 
-server.use(router);
+// Uncomment to allow write operations
+const fs = require("fs")
+const path = require("path")
+const db = JSON.parse(fs.readFileSync(path.join("db.json")))
+const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
+    console.log('JSON Server is running')
+})
+
+// // Export the Server API
+// module.exports = server
+
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json'); // Your database file
+// const middlewares = jsonServer.defaults();
+
+// // Add custom middleware for CORS
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+// server.use(router);
+// server.listen(3000, () => {
+//   console.log('JSON Server is running');
+// });

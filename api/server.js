254b0039f8d930863e130eea94f5d009102fa7d7
sanzdecastro <sanzdecastro@gmail.com>
2024-04-27 16:58:27 +0200

    fix

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 import { create, router as _router, defaults } from 'json-server'
 
 const server = create()
-const router = _router('data.json')
+const router = _router('db.json')
 const middlewares = defaults()
 
 server.use(middlewares)

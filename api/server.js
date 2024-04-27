976b1f5e371fff450be6fdb35fd2b86f8a1664d1
sanzdecastro <sanzdecastro@gmail.com>
2024-04-27 21:21:44 +0200

    hotfix

--- api/server.js
+++ api/server.js
@@ -4,9 +4,11 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-const fs = require("fs")
-const path = require("path")
-const db = JSON.parse(fs.readFileSync(path.join("db.json")))
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
 const router = jsonServer.router(db)
 
 // Comment out to allow write operations

8430d15b72030cb69cf806c79fda70ba591c2889
sanzdecastro <sanzdecastro@gmail.com>
2024-04-27 17:16:47 +0200

    fix

--- api/server.js
+++ api/server.js
@@ -1,13 +1,24 @@
-import { create, router as _router, defaults } from 'json-server'
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
 
-const server = create()
-const router = _router('db.json')
-const middlewares = defaults()
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`)
-})
+// Export the Server API
+module.exports = server;
